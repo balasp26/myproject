4289f6dc07369c379e6edf8c2f11e700858cc968
Balasubramaniyam Prabakaran <balasubramaniyam.prabakaran@oracle.com>
2020-04-28 00:16:00 +0530

    Adding regex1.py

---
+++ My Training/regex1.py
@@ -0,0 +1,10 @@
+import re
+
+text_to search = '''
+abcdef
+
+'''
+
+# r Represents the Raw String which wiull print the whole string as it is.
+print("\ttab") #with out making the string raw
+print(r"\ttab") # Making the string Raw
