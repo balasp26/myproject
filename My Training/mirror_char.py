e15bfaf116742c75b63a218a5c35ee90f1674141
bprabaka <balasp26@gmail.com>
2020-05-03 00:43:58 +0530

    Adding For loops

---
+++ My Training/mirror_char.py
@@ -0,0 +1,33 @@
+# function to mirror characters of a string
+
+def mirrorChars(input, k):
+	# create dictionary
+	original = 'abcdefghijklmnopqrstuvwxyz'
+	print("The Original is {}".format(original))
+	reverse = 'zyxwvutsrqponmlkjihgfedcba'
+	print("The Reversal is {}".format(reverse))
+	dictChars = dict(zip(original, reverse))
+	print("The Dictchars are {}".format(dictChars))
+
+	# separate out string after length k to change
+	# characters in mirror
+	prefix = input[0:k - 1]
+	print("The Prefix is {}".format(prefix))
+	suffix = input[k - 1:]
+	print("The Suffix is {}".format(suffix))
+	mirror = ''
+
+	# change into mirror
+	for i in range(0, len(suffix)):
+		mirror = mirror + dictChars[suffix[i]]
+		print("The Mirror is {}".format(mirror))
+
+	# concat prefix and mirrored part
+	print(prefix + mirror)
+
+
+# Driver program
+if __name__ == "__main__":
+	input = 'paradox'
+	k = 3
+	mirrorChars(input, k)
--- My Training/regex1.py
+++ My Training/regex1.py
@@ -1,10 +1,41 @@
 import re
 
-text_to search = '''
-abcdef
 
+text_to_search = '''
+abcdefghijklmnopqurtuvwxyz
+ABCDEFGHIJKLMNOPQRSTUVWXYZ
+
+1234567890
+
+Ha HaHa
+
+MetaCharacters (Need to be escaped):
+. ^ $ * + ? { } [ ] \ | ( )
+
+coreyms.com
+
+321-555-4321
+123.555.1234
+123*555*1234
+800-555-1234
+900-555-1234
+
+Mr. Abdul Kalam
+Mr Kamarajar
+Mr Shakthiman
+Mrs. Doubtfire
+Mr. B
 '''
+
+sentence = 'Start a sentence and then bring it to an end'
 
 # r Represents the Raw String which wiull print the whole string as it is.
 print("\ttab") #with out making the string raw
 print(r"\ttab") # Making the string Raw
+
+pattern = (r'abcd')
+
+matches = pattern.find(text_to_search)
+
+for match in matches:
+	print(match)
